f1083501f05275f176b169df7f0e527c986cc6c3
Nikolay Astakhov <astakhov.na@gmail.com>
2017-10-12 22:31:48 +0300

    Init and serve scripts

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+export GOPATH=`pwd`
+
+go get github.com/gin-gonic/gin
+go get github.com/lib/pq
+go get -u github.com/jinzhu/gorm
---
+++ serve.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+export GOPATH=`pwd`
+
+go run src/server/main.go
