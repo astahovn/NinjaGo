ef7a82330a51fcc778ce739b45da795a3a38dfa5
Nikolay Astakhov <astakhov.na@gmail.com>
2017-11-07 23:13:03 +0300

    Database configuration file

--- lib/db/db.go
+++ lib/db/db.go
@@ -3,16 +3,42 @@ package db
 import (
   "github.com/jinzhu/gorm"
   _ "github.com/jinzhu/gorm/dialects/postgres"
+  "os"
+  "encoding/json"
+  "fmt"
 )
+
+const ConfigFile = "config/db.json"
+
+type Configuration struct {
+  Database string
+  Host string
+  Username string
+  Password string
+}
 
 var db *gorm.DB
 
 func Init() *gorm.DB {
-  db, err := gorm.Open("postgres", "host=localhost user=postgres dbname=my_habr sslmode=disable password=postgres")
+  config := ReadConfiguration()
+  dbConfigString := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", config.Host, config.Username, config.Database, config.Password)
+
+  db, err := gorm.Open("postgres", dbConfigString)
   if err != nil {
     panic("failed to connect database")
   }
   return db
+}
+
+func ReadConfiguration() Configuration {
+  file, _ := os.Open(ConfigFile)
+  decoder := json.NewDecoder(file)
+  configuration := Configuration{}
+  err := decoder.Decode(&configuration)
+  if err != nil {
+    panic("failed to load database configuration file db.json")
+  }
+  return configuration
 }
 
 func GetInstance() *gorm.DB {
